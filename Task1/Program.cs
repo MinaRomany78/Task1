c4507d94f9cffb7c90e92b5fe7f978e563aefe25
MinaRomany78 <minaromany155@gmail.com>
2025-03-02 17:11:32 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.WriteLine("Number of small carpets:");
+            int sNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number of large carpets:");
+            int lNum = Convert.ToInt32(Console.ReadLine());
+            int Cost = sNum * 25 + lNum * 35;
+            Console.WriteLine("Price per small room: $25");
+            Console.WriteLine("Price per large room: $35");
+            Console.WriteLine($"Cost : ${Cost} ");
+            double Tax = Convert.ToDouble(Cost )* 6 / 100;
+            Console.WriteLine($"Tax : ${Tax} ");
+            Console.WriteLine("================================");
+            double Total =Convert.ToDouble( Tax + Cost);
+            Console.WriteLine($"Total estimate : ${Total} ");
+            Console.WriteLine("This estimate is valid for 30 days") ;
+
+
+
+
+        }
+    }
+}
